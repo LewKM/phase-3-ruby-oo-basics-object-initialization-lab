b5830575bdd3013d2fa65978ca6f850606438676
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-08-16 22:47:31 +0300

    commit tree 202799ec6d8f8b5de6c1262689c8359fdbe88935

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+    attr_accessor :name
+    attr_reader :breed
+
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+end
+
+# lassie = Dog.new("German Shepherd")
+# puts lassie.breed
